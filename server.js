444e17e03ac96931503a8ea5b7347caabdb29ec9
sskiles66 <stephentskiles@hotmail.com>
2023-10-11 23:29:06 -0600

    next

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
@@ -26,11 +28,11 @@ app.set("layout", "./layouts/layout")
  * Routes
  *************************/
 app.use(static)
-
+app.use("/inv", inventoryRoute)
 // Index Route
-app.get("/", function (req, res){
-  res.render("index", {title: "Home"})
-})
+
+//The route will lead to a controller
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
